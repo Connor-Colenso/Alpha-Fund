08f60e8ce1a30b0fff8658e57bff8569ecf3a2ec
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2022-03-07 09:54:13 +0000

    Refactor.

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ import pandas as pd
 from utility.current_allocation import current_allocation
 from utility.liquidate import liquidate
 
-if __name__ == '__main__':
-    portfolio_cash = 100000
 
+def graph_portfolio():
+
+    # Defined and fixed for our use case.
+    portfolio_cash = 100000
     start_date = '2022-02-24'
 
+    # Import the spreadsheet.
     df = pd.read_csv('Alpha Fund - Sheet1.csv')
+
+    # Perform calculations.
     portfolio_cash, asset_dictionary = current_allocation(portfolio_cash, df)
     portfolio_value, assets_over_time = liquidate(start_date, asset_dictionary)
-
-    portfolio_liquidation_value = portfolio_cash + portfolio_value
 
     plt.plot(portfolio_cash + assets_over_time)
     plt.xticks(rotation=45)
@@ -22,3 +25,9 @@ if __name__ == '__main__':
     plt.ylabel('Price (USD $)')
     plt.title('Alpha Fund Portfolio')
     plt.show()
+
+
+if __name__ == '__main__':
+
+    graph_portfolio()
+
