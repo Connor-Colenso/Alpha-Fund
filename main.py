d611381af8edb0cb4540a448015616042182b7d3
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2022-03-12 23:25:31 +0000

    Remove pie chart shadow.

--- main.py
+++ main.py
@@ -243,7 +243,7 @@ class Portfolio:
                 labels.append(column)
                 values.append(df[column][-1])
 
-        plt.pie(x=values, labels=labels, autopct='%1.1f%%', shadow=True)
+        plt.pie(x=values, labels=labels, autopct='%1.1f%%')
         plt.title(name)
         plt.savefig(f'{name} - pie chart.png', dpi=100)
         plt.show()
